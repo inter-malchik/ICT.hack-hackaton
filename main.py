48cbcce4ba07a160a5aec89b2f27a2f7f054a5ab
Anton Subbota <66995955+Anton2813@users.noreply.github.com>
2021-12-18 18:00:30 +0300

    branch fix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import telebot
 from config import TOKEN
-from checkargs import *
+from db_requests import *
+
 
 class Question:
     def __init__(self, chat_id):
@@ -16,70 +17,53 @@ class Question:
         print(self.surname)
         print(self.ISU)
         print(self.is_teacher)
+        add_user(self.chat_id, self.name, self.surname, self.ISU)
+        if self.is_teacher:
+            add_teacher(self.chat_id)
+        else:
+            add_student(self.chat_id)
+
+
 
 bot = telebot.TeleBot(f'{TOKEN}', parse_mode=False)
 
 registration = {}
 
-
 @bot.message_handler(commands=['start'])
 def welcome(message):
     bot.send_message(message.chat.id, "Привет! Я бот для обучения с элементами геймификаций.")
-    registration[message.chat.id] = Question(message.chat.id)
-    msg = bot.send_message(message.chat.id, 'Зарегистрируйся. Введи свое имя')
+    user_answer = Question(message.chat.id)
+    registration[message.chat.id] = user_answer
+    msg = bot.send_message(user_answer.chat_id, 'Зарегистрируйся. Введи свое имя')
     bot.register_next_step_handler(msg, process_registration_name)
 
-
 def process_registration_name(message):
-    user_data = registration[message.chat.id]
-    user_answer = clear_argument(message.text)
-    if check_name(user_answer):
-        user_data.name = user_answer
-        msg = bot.send_message(user_data.chat_id, 'Введите фамилию')
-        bot.register_next_step_handler(msg, process_registration_surname)
-    else:
-        msg = bot.send_message(user_data.chat_id, 'некорректный ввод: введите еще раз')
-        bot.register_next_step_handler(msg, process_registration_name)
-
+    user_answer = registration[message.chat.id]
+    user_answer.name = message.text
+    msg = bot.send_message(user_answer.chat_id, 'Введите фамилию')
+    bot.register_next_step_handler(msg, process_registration_surname)
 
 def process_registration_surname(message):
-    user_data = registration[message.chat.id]
-    user_answer = clear_argument(message.text)
-    if check_name(user_answer):
-        user_data.surname = user_answer
-        msg = bot.send_message(user_data.chat_id, 'Введите ИСУ номер')
-        bot.register_next_step_handler(msg, process_registration_ISU)
-    else:
-        msg = bot.send_message(user_data.chat_id, 'некорректный ввод: введите еще раз')
-        bot.register_next_step_handler(msg, process_registration_surname)
-
+    user_answer = registration[message.chat.id]
+    user_answer.surname = message.text
+    msg = bot.send_message(user_answer.chat_id, 'Введите ISU номер')
+    bot.register_next_step_handler(msg, process_registration_ISU)
 
 def process_registration_ISU(message):
-    user_data = registration[message.chat.id]
-    user_answer = clear_argument(message.text)
-    if check_num(user_answer):
-        user_data.isu = user_answer
-        msg = bot.send_message(user_data.chat_id, 'Вы учитель? Да/Нет')
-        bot.register_next_step_handler(msg, process_registration_position)
-    else:
-        msg = bot.send_message(user_data.chat_id, 'некорректный ввод: введите еще раз')
-        bot.register_next_step_handler(msg, process_registration_ISU)
+    user_answer = registration[message.chat.id]
+    user_answer.ISU = message.text
+    msg = bot.send_message(user_answer.chat_id, 'Вы учитель? Да/Нет')
+    bot.register_next_step_handler(msg, process_registration_is_teacher)
 
 
-def process_registration_position(message):
-    user_data = registration[message.chat.id]
-    user_answer = clear_argument(message.text)
-    if user_answer == "Да":
-        user_data.is_teacher = True
-        del registration[user_data.chat_id]
-        return
-    elif user_answer == "Нет":
-        user_data.is_teacher = False
-        del registration[user_data.chat_id]
-        return
-    else:
-        msg = bot.send_message(user_data.chat_id, 'некорректный ввод: введите еще раз')
-        bot.register_next_step_handler(msg, process_registration_position)
-
+def process_registration_is_teacher(message):
+    user_answer = registration[message.chat.id]
+    answer = message.text
+    if answer == 'Да':
+        user_answer.is_teacher = True
+    elif answer == 'Нет':
+        user_answer.is_teacher = False
+    del registration[user_answer.chat_id]
 
 bot.infinity_polling()
+
